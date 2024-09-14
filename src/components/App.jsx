c943eb28a29b491d35c66b3179cfd5a6ef19adf9
ogoncharenko32 <krapniknil32@gmail.com>
2024-09-14 18:14:46 +0300

    task 2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import Profile from "./Profile/Profile";
+import FriendList from "./Friendlist/Friendlist";
 import userData from "../userData.json"
+import friends from "../friends.json"
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </>
   );
 };
